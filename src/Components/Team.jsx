82fca3c547e1e38b3358d8f152a1b62a1f03cfc5
Realrickkel <77985377+Realrickkel@users.noreply.github.com>
2024-12-12 16:17:34 +0100

    update

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from "react"
-import { ACTRESS2IMG, ACTRESS3IMG, ACTRESSIMG, BILLLIEIMG, INSTABGBOTIMG, INSTABGTOPIMG, IRENEIMG, ITZYIMG, JOYIMG, LIAIMG, MINJIIMG, PIXXIE2IMG, PIXXIE3IMG, PIXXIECUT, PIXXIEIMG, SIYEONIMG, SJUKSEEIMG, SUNMIIMG, YUKIIMG } from "../utils"
+import { ACTRESS2IMG, ACTRESS3IMG, ACTRESSIMG, BILLLIEIMG, DADIMG, DADSMALLIMG, GEUNSOOIMG, GEUNSOOSMALLIMG, HYEONIIMG, HYEONISMALLIMG, INSTABGBOTIMG, INSTABGTOPIMG, IRENEIMG, ITZYIMG, JOYIMG, LIAIMG, MINJIIMG, PIXXIE2IMG, PIXXIE3IMG, PIXXIECUT, PIXXIEIMG, SAEROYISIMG, SAEROYISMALLIMG, SEUNGGWONIMG, SEUNGGWONSMALLIMG, SIYEONIMG, SJUKSEEIMG, SOOAHIMG, SOOAHSMALLIMG, SUNMIIMG, TONIIMG, TONISMALLIMG, YISEOIMG, YISEOSMALLIMG, YUKIIMG } from "../utils"
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 
 const Team = () => {
-    const [picture, setPicture] = useState(SJUKSEEIMG)
+    const [picture, setPicture] = useState(SAEROYISIMG)
     const [pictureSmall, setPictureSmall] = useState(ACTRESSIMG)
-    const [text, setText] = useState('Billlie')
+    const [text, setText] = useState('Saeroyi')
     const [picCase, setPicCase] = useState('first')
     const [classnameFunc, setClassnameFunc] = useState("blueClass")
     const [bgImage,setBgImage] = useState('bg_image_blue')
+    const [classnameMessage1Func,setClassnameMessage1Func] = useState('blue_first_message')
+    const [classnameMessage2Func,setClassnameMessage2Func] = useState('blue_second_message')
+    const [classnameMessage3Func,setClassnameMessage3Func] = useState('blue_third_message')
+    const [classSmallPic,setClassSmallPic] = useState('blue_small_image')
+    const [txtMes1,setTxtMes1] = useState('#leader')
+    const [txtMes2,setTxtMes2] = useState("He's the leader of the bunch,")
+    const [txtMes3,setTxtMes3] = useState('you know him well')
+    const [txtMes4,setTxtMes4] = useState('Never skip a meal')
+    const [animateOut, setAnimateOut] = useState(false)
+    const [animateIn, setAnimateIn] = useState(true)
+    const [animateIn2, setAnimateIn2] = useState(false)
 
     useGSAP(() => {
         var tl = gsap.timeline({repeat: -1,});
@@ -42,78 +53,187 @@ const Team = () => {
 
     },[])
 
+    useGSAP(() => {
+        if(animateOut == true){
+            gsap.to("#big_group" , {
+                opacity: 0,
+                duration: 0.5,
+                ease: "power4.inOut"
+            })
+
+            gsap.to("#asjemenou", {
+                backgroundColor: 'rgba(0,0,0,100)',
+                duration: 0.5,
+                ease: "power4.inOut"
+            })
+
+            console.log(animateOut)
+            setTimeout(function () {
+                setAnimateIn(true)
+            }, 500);
+            setAnimateOut(false)
+        }
+    },[animateOut])
+
+    useGSAP(() => {
+        if(animateIn2 == true){
+            gsap.to("#big_group" , {
+                opacity: 1,
+                duration: 0.5,
+                ease: "power4.inOut"
+            })
+
+            gsap.to("#asjemenou", {
+                backgroundColor: 'rgba(0,0,0,0)',
+                duration: 0.5,
+                ease: "power4.inOut"
+            })
+
+            setAnimateIn2(false)
+        }
+    },[animateIn2])
+
     
     useEffect(() => {
+        if(animateIn == true){
         switch(picCase){
             case 'first': 
-                setPicture(BILLLIEIMG)
-                setText('Billlie')
+                setPicture(SAEROYISIMG)
+                setText('Saeroyi')
                 setClassnameFunc("blueClass")
-                setPictureSmall(ACTRESSIMG)
+                setPictureSmall(SAEROYISMALLIMG)
                 setBgImage('bg_image_blue')
+                setClassnameMessage1Func('blue_first_message')
+                setClassnameMessage2Func('blue_second_message')
+                setClassnameMessage3Func('blue_third_message')
+                setTxtMes1('#leader')
+                setTxtMes2("He's the leader of the bunch,")
+                setTxtMes3('you know him well')
+                setTxtMes4('Revenge is Sweet')
+                setClassSmallPic('blue_small_image')
                 break
             case 'second': 
-                setPicture(IRENEIMG)
-                setText('Irene')
+                setPicture(YISEOIMG)
+                setText('Yiseo')
                 setClassnameFunc("redClass")
-                setPictureSmall(ACTRESS2IMG)
+                setPictureSmall(YISEOSMALLIMG)
                 setBgImage('bg_image_red')
+                setClassnameMessage1Func('red_first_message')
+                setClassnameMessage2Func('red_second_message')
+                setClassnameMessage3Func('red_third_message')
+                setTxtMes1('#leader')
+                setTxtMes2("He's the leader of the bunch,")
+                setTxtMes3('you know him well')
+                setTxtMes4('Never skip a meal')
+                setClassSmallPic('red_small_image')
                 break
             case 'third': 
-                setPicture(PIXXIECUT)
-                setText('Pixxie')
-                setClassnameFunc("blueClass")
-                setPictureSmall(ACTRESS3IMG)
-                setBgImage('bg_image_blue')
+                setPicture(SOOAHIMG)
+                setText('SooAh')
+                setClassnameFunc("yellowClass")
+                setPictureSmall(SOOAHSMALLIMG)
+                setBgImage('bg_image_yellow')
+                setClassnameMessage1Func('yellow_first_message')
+                setClassnameMessage2Func('yellow_second_message')
+                setClassnameMessage3Func('yellow_third_message')
+                setTxtMes1('#leader')
+                setTxtMes2("He's the leader of the bunch,")
+                setTxtMes3('you know him well')
+                setTxtMes4('Never skip a meal')
+                setClassSmallPic('yellow_small_image')
                 break
             case 'fourth': 
-                setPicture(PIXXIE2IMG)
-                setText('Pixxie 2')
-                setClassnameFunc("yellowClass")
-                setPictureSmall(JOYIMG)
-                setBgImage('bg_image_yellow')
+                setPicture(HYEONIIMG)
+                setText('Hyeoni')
+                setClassnameFunc("blueClass")
+                setPictureSmall(HYEONISMALLIMG)
+                setBgImage('bg_image_blue')
+                setClassnameMessage1Func('blue_first_message')
+                setClassnameMessage2Func('blue_second_message')
+                setClassnameMessage3Func('blue_third_message')
+                setTxtMes1('#leader')
+                setTxtMes2("He's the leader of the bunch,")
+                setTxtMes3('you know him well')
+                setTxtMes4('Never skip a meal')
+                setClassSmallPic('blue_small_image')
                 break
             case 'fifth': 
-                setPicture(PIXXIE3IMG)
-                setText('Pixxie 3')
+                setPicture(SEUNGGWONIMG)
+                setText('Seunggwon')
                 setClassnameFunc("redClass")
-                setPictureSmall(ITZYIMG)
+                setPictureSmall(SEUNGGWONSMALLIMG)
                 setBgImage('bg_image_red')
+                setClassnameMessage1Func('red_first_message')
+                setClassnameMessage2Func('red_second_message')
+                setClassnameMessage3Func('red_third_message')
+                setTxtMes1('#leader')
+                setTxtMes2("He's the leader of the bunch,")
+                setTxtMes3('you know him well')
+                setTxtMes4('Never skip a meal')
+                setClassSmallPic('red_small_image')
                 break
             case 'sixth': 
-                setPicture(SIYEONIMG)
-                setText('Siyeon')
+                setPicture(GEUNSOOIMG)
+                setText('Geunsoo')
                 setClassnameFunc("yellowClass")
-                setPictureSmall(LIAIMG)
+                setPictureSmall(GEUNSOOSMALLIMG)
                 setBgImage('bg_image_yellow')
+                setClassnameMessage1Func('yellow_first_message')
+                setClassnameMessage2Func('yellow_second_message')
+                setClassnameMessage3Func('yellow_third_message')
+                setTxtMes1('#leader')
+                setTxtMes2("He's the leader of the bunch,")
+                setTxtMes3('you know him well')
+                setTxtMes4('Never skip a meal')
+                setClassSmallPic('yellow_small_image')
                 break
             case 'seventh': 
-                setPicture(SJUKSEEIMG)
-                setText('Sekyung')
+                setPicture(TONIIMG)
+                setText('Toni')
                 setClassnameFunc("blueClass")
-                setPictureSmall(MINJIIMG)
+                setPictureSmall(TONISMALLIMG)
                 setBgImage('bg_image_blue')
+                setClassnameMessage1Func('blue_first_message')
+                setClassnameMessage2Func('blue_second_message')
+                setClassnameMessage3Func('blue_third_message')
+                setTxtMes1('#leader')
+                setTxtMes2("He's the leader of the bunch,")
+                setTxtMes3('you know him well')
+                setTxtMes4('Never skip a meal')
+                setClassSmallPic('blue_small_image')
                 break
             case 'eight': 
-                setPicture(YUKIIMG)
-                setText('Yuki')
-                setClassnameFunc("yellowClass")
-                setPictureSmall(SUNMIIMG)
-                setBgImage('bg_image_yellow')
+                setPicture(DADIMG)
+                setText('Mr. Park')
+                setClassnameFunc("redClass")
+                setPictureSmall(DADSMALLIMG)
+                setBgImage('bg_image_red')
+                setClassnameMessage1Func('red_first_message')
+                setClassnameMessage2Func('red_second_message')
+                setClassnameMessage3Func('red_third_message')
+                setTxtMes1('#leader')
+                setTxtMes2("He's the leader of the bunch,")
+                setTxtMes3('you know him well')
+                setTxtMes4('Never skip a meal')
+                setClassSmallPic('red_small_image')
                 break
         }
-    },[picCase])
+        setAnimateIn(false)
+        setAnimateIn2(true)
+    }
+    },[animateIn])
 
   return (
-    <section className={bgImage}>
+    <section id="asjemenou" className={bgImage}>
         <div className="flex justify-center pt-8">
+            
             <div className="flex justify-start bg-amber-500 w-[90vw] sm:w-[576px] md:w-[700px] lg:w-[950px] xl:w-[1180px] 2xl:w-[1400px]">
                 <h2 className="text-7xl font-bold">Meet the team!</h2>
             </div>
         </div>
         <div className="flex justify-center">
             <div className="max-w-[90vw] md:max-w-[92vw] flex lg:flex-row flex-col justify-between md:gap-6 lg:gap-10 xl:gap-16 items-start py-[10vh] pt-[4vh]">
-                <div className="relative screen-max-width flex flex-col justify-center items-center bg-pink-400 sm:bg-amber-400 md:bg-violet-900 lg:bg-orange-600 xl:bg-teal-500 2xl:bg-green-800 min-w-[90vw] sm:min-w-[576px] md:min-w-[700px] lg:min-w-[760px] xl:min-w-[800px] 2xl:min-w-[960px] md:h-[740px] overflow-hidden">
+                <div id="big_group" className="relative opacity-0 screen-max-width flex flex-col justify-center items-center min-w-[90vw] sm:min-w-[576px] md:min-w-[700px] lg:min-w-[760px] xl:min-w-[800px] 2xl:min-w-[960px] md:h-[740px] overflow-hidden">
                     <div className="">
                         <div className="flex justify-center w-[90vw] sm:w-[576px] md:w-[700px] lg:w-[760px] xl:w-[800px] 2xl:w-[960px] md:h-[740px] overflow-hidden">
                             <img src={picture} alt="MAIN IMG" id="" className="object-contain max-h-full max-w-full z-10 mb-12"/>
@@ -127,7 +247,7 @@ const Team = () => {
                         <div className={classnameFunc}>
                             <p className="font-semibold text-9xl z-50">{text}</p>
                         </div>
-                        <div id="pic_small" className="absolute md:scale-75 lg:scale-90 xl:scale-95 2xl:scale-100 md:flex hidden 2xl:top-16 2xl:right-12 md:top-8 md:right-6 w-[230px] h-[300px] rounded-2xl items-end shadow-lg shadow-zinc-800 z-30">
+                        <div id="pic_small" className={classSmallPic}>
                             <div className="absolute top-0 left-0 h-[41px] w-full rounded-t-2xl bg-white"></div>
                             <div className="flex flex-row gap-[3px] absolute top-[19px] right-4">
                                 <div className="h-[5px] w-[5px] rounded-full bg-gray-600"></div>
@@ -138,47 +258,49 @@ const Team = () => {
                                 <img src={pictureSmall} alt="MAIN IMG" id="" className=" h-[260px] w-full object-cover object-top"/>
                             </div>
                         </div>
-                        <div className="flex items-center justify-center top-48 left-28 absolute w-[150px] h-[50px] bg-white rounded-2xl shadow-lg shadow-zinc-800 p-2">
-                            <p className="text-sm text-black">#Leader</p>
+                        <div className={classnameMessage1Func}>
+                            <p className="text-sm text-black font-light">{txtMes1}</p>
                         </div>
-                        <div id="textGrow" className="flex flex-wrap items-center justify-center bottom-56 left-72 absolute w-[230px] h-[70px] bg-white rounded-2xl shadow-lg shadow-zinc-800 p-2 z-40">
-                            <p className="text-xs text-black"><span className="text-sm">He's the leader of the bunch,</span> you know him well</p>
+                        <div id="textGrow" className={classnameMessage2Func}>
+                            <p className="text-xs text-black font-light"><span className="text-sm font-light">{txtMes2}</span><br/>{txtMes3}</p>
                         </div>
-                        <div className="flex items-center justify-center bottom-48 right-32 absolute w-[150px] h-[50px] bg-white rounded-2xl shadow-lg shadow-zinc-800 p-2">
-                            <p className="text-sm text-black">Never skip a meal</p>
+                        <div className={classnameMessage3Func}>
+                            <p className="text-sm text-black font-light">{txtMes4}</p>
                         </div>
+                        
                     </div>
                 </div>
                 <div className="person-selector md:inline-block hidden lg:overflow-y-scroll xl:overflow-hidden min-h-0 lg:h-[740px]">
                     <div className="flex flex-row flex-wrap justify-center lg:flex-nowrap lg:flex-col xl:flex-row xl:flex-wrap bg-red-700 gap-4 lg:gap-4 md:max-w-[700px] 2xl:max-w-[376px] xl:max-w-[316px]">
-                        <div onClick={() => setPicCase('first')} className="personblock">
+                        <div onClick={() => { setPicCase('first'); setAnimateOut(true)}} className="personblock">
                             <img src={BILLLIEIMG} alt="BILLLIE IMG"  id="" className="object-contain max-h-full max-w-full"/>
                         </div>
-                        <div onClick={() => setPicCase('second')} className="personblock lg:mt-12 xl:mt-0">
+                        <div onClick={() => { setPicCase('second'); setAnimateOut(true)}} className="personblock lg:mt-12 xl:mt-0">
                             <img src={IRENEIMG} alt="IRENE IMG"  id="" className="object-contain max-h-full max-w-full"/>
                         </div>
-                        <div onClick={() => setPicCase('third')} className="personblock lg:mt-12">
+                        <div onClick={() => { setPicCase('third'); setAnimateOut(true)}} className="personblock lg:mt-12">
                             <img src={PIXXIEIMG} alt="PIXXIE IMG"  id="" className="object-contain max-h-full max-w-full"/>
                         </div>
-                        <div onClick={() => setPicCase('fourth')} className="personblock lg:mt-12">
+                        <div onClick={() => { setPicCase('fourth'); setAnimateOut(true)}} className="personblock lg:mt-12">
                             <img src={PIXXIE2IMG} alt="PIXXIE 2 IMG"  id="" className="object-contain max-h-full max-w-full"/>
                         </div>
-                        <div onClick={() => setPicCase('fifth')} className="personblock lg:mt-12">
+                        <div onClick={() => { setPicCase('fifth'); setAnimateOut(true)}} className="personblock lg:mt-12">
                             <img src={PIXXIE3IMG} alt="PIXXIE 3 IMG"  id="" className="object-contain max-h-full max-w-full"/>
                         </div>
-                        <div onClick={() => setPicCase('sixth')} className="personblock lg:mt-12">
+                        <div onClick={() => { setPicCase('sixth'); setAnimateOut(true)}} className="personblock lg:mt-12">
                             <img src={SIYEONIMG} alt="SIYEON IMG"  id="" className="object-contain max-h-full max-w-full"/>
                         </div>
-                        <div onClick={() => setPicCase('seventh')} className="personblock lg:mt-12">
+                        <div onClick={() => { setPicCase('seventh'); setAnimateOut(true)}} className="personblock lg:mt-12">
                             <img src={SJUKSEEIMG} alt="SJUKSEE IMG"  id="" className="object-contain max-h-full max-w-full"/>
                         </div>
-                        <div onClick={() => setPicCase('eight')} className="personblock lg:mt-12">
+                        <div onClick={() => { setPicCase('eight'); setAnimateOut(true)}} className="personblock lg:mt-12">
                             <img src={YUKIIMG} alt="YUKI IMG"  id="" className="object-contain max-h-full max-w-full"/>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        
     </section>
   )
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,11 @@
 import instabgbotimg from "/assets/images/Instagram_background_bot.webp";
 import instabgtopimg from "/assets/images/Instagram_background_top.webp";
 
+import BG1img from "/assets/images/interior1v2WEBP.webp";
+import BG2img from "/assets/images/interior2v2WEBP.webp";
+import BG3img from "/assets/images/interior3v2WEBP.webp";
+
+
 import billlieimg from "/assets/images/billlie.webp";
 import Ireneimg from "/assets/images/Irene.webp";
 import Pixxieimg from "/assets/images/Pixxie.webp";
@@ -11,6 +16,15 @@ import Siyeonimg from "/assets/images/siyeon.webp";
 import Sjukseeimg from "/assets/images/sjuksee.webp";
 import Yukiimg from "/assets/images/Yuki.webp";
 
+import Saeroyiimg from "/assets/images/SaeroyiWEBP.webp";
+import Yiseoimg from "/assets/images/YiseoWEBP.webp";
+import Sooahimg from "/assets/images/SooahWEBP.webp";
+import Hyeoniimg from "/assets/images/HyeoniWEBP.webp";
+import Seunggwonimg from "/assets/images/SeunggwonWEBP.webp";
+import Geunsooimg from "/assets/images/GeunsooWEBP.webp";
+import Toniimg from "/assets/images/ToniWEBP.webp";
+import Dadmimg from "/assets/images/DadWEBP.webp";
+
 import Actressimg from "/assets/images/Actress.webp";
 import Actress2img from "/assets/images/Actress2WEBP.webp";
 import Actress3img from "/assets/images/Actress3WEBP.webp";
@@ -20,8 +34,21 @@ import Liaimg from "/assets/images/LiaWEBP.webp";
 import Minjiimg from "/assets/images/MinjiWEBP.webp";
 import Sunmimg from "/assets/images/SunmiWEBP.webp";
 
+import SaeroyiSmallimg from "/assets/images/Saeroyi2WEBP.webp";
+import YiseoSmallimg from "/assets/images/Yiseo2WEBP.webp";
+import SooAhSmallimg from "/assets/images/SooAh2WEBP.webp";
+import HyeoniSmallimg from "/assets/images/Hyeoni2WEBP.webp";
+import SeunggwonSmallimg from "/assets/images/Seunggwon2WEBP.webp";
+import GeunsooSmallimg from "/assets/images/Geunsoo2WEBP.webp";
+import ToniSmallimg from "/assets/images/Toni2WEBP.webp";
+import DadSmallimg from "/assets/images/Appa2WEBP.webp";
+
 export const INSTABGBOTIMG = instabgbotimg;
 export const INSTABGTOPIMG = instabgtopimg;
+
+export const BG1IMG = BG1img;
+export const BG2IMG = BG2img;
+export const BG3IMG = BG3img;
 
 export const BILLLIEIMG = billlieimg;
 export const IRENEIMG = Ireneimg;
@@ -33,6 +60,15 @@ export const SIYEONIMG = Siyeonimg;
 export const SJUKSEEIMG = Sjukseeimg;
 export const YUKIIMG = Yukiimg;
 
+export const SAEROYISIMG = Saeroyiimg;
+export const YISEOIMG = Yiseoimg;
+export const SOOAHIMG = Sooahimg;
+export const HYEONIIMG = Hyeoniimg;
+export const SEUNGGWONIMG = Seunggwonimg;
+export const GEUNSOOIMG = Geunsooimg;
+export const TONIIMG = Toniimg;
+export const DADIMG = Dadmimg;
+
 export const ACTRESSIMG = Actressimg;
 export const ACTRESS2IMG = Actress2img;
 export const ACTRESS3IMG = Actress3img;
@@ -41,3 +77,12 @@ export const ITZYIMG = ITZYimg;
 export const LIAIMG = Liaimg;
 export const MINJIIMG = Minjiimg;
 export const SUNMIIMG = Sunmimg;
+
+export const SAEROYISMALLIMG = SaeroyiSmallimg;
+export const YISEOSMALLIMG = YiseoSmallimg;
+export const SOOAHSMALLIMG = SooAhSmallimg;
+export const HYEONISMALLIMG = HyeoniSmallimg;
+export const SEUNGGWONSMALLIMG = SeunggwonSmallimg;
+export const GEUNSOOSMALLIMG = GeunsooSmallimg;
+export const TONISMALLIMG = ToniSmallimg;
+export const DADSMALLIMG = DadSmallimg;
