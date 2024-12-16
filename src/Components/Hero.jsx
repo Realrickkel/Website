ae642418df69b32cd67c69a9cbdab70d2a4d9efb
Realrickkel <77985377+Realrickkel@users.noreply.github.com>
2024-12-16 12:01:21 +0100

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,11 +4,17 @@ const Hero = () => {
         <div className="flex justify-center">
             <div className="flex justify-center max-w-[90vw] md:max-w-[76vw]">
                 <div className="screen-max-width">
-                    <div className="flex flex-col justify-center h-screen items-center">
-                        <h2 className="text-6xl text-white">
-                            Welcome to:
-                        </h2>
-                        <h1 className="text-9xl text-[#7c0aff]">DanBam</h1>
+                    <div className="flex flex-col justify-between h-screen items-center p-20">
+                        <div className="flex flex-col justify-center items-center h-[70vh]">
+                            <h2 className="text-6xl text-white">
+                                Welcome to:
+                            </h2>
+                            <h1 className="text-9xl text-[#7c0aff]">DanBam</h1>
+                        </div>
+                        <div className="flex justify-center items-center flex-col">
+                            <p>Demo</p>
+                            <p className="rotate-180">^</p>
+                        </div>
                     </div>
                 </div>
             </div>
